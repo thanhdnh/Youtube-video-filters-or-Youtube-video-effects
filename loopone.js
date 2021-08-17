588392c2c010e2e197978c94f177d5bd9c6f9b37
HTCom <myhoangthanh@yahoo.com>
2021-08-17 10:44:38 +0700

    Update loopone.js

--- loopone.js
+++ loopone.js
@@ -7,18 +7,11 @@ function loopOneVideo() {
 			if (localStorage.getItem('youtube_filter_video_auto_loop_current_track') != null && localStorage.getItem('youtube_filter_video_auto_loop_current_track') !== undefined)
 				loopenable = (localStorage.getItem('youtube_filter_video_auto_loop_current_track') == 'true' ? true : false);
 
-			if (state === 0 && loopenable) {
-				/*var replays = document.querySelectorAll('#player #player-container #movie_player .ytp-chrome-bottom .ytp-left-controls .ytp-play-button.ytp-button');
-				if(replays.length>0)
-					replays[0].click();*/
+			if (state === 0 && loopenable) 
 				player.seekTo(0);
-			}
-			if (state === 0 && !loopenable) {
-				/*var nextvids = document.querySelectorAll('#player #player-container #movie_player .ytp-chrome-bottom .ytp-left-controls .ytp-next-button.ytp-button');
-				if(nextvids.length>0)
-					nextvids[0].click();*/
+		
+			if (state === 0 && !loopenable) 
 				player.nextVideo.call();
-			}
 		});
 	}
 }
